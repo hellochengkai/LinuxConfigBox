f30eb5a3ae94b267baecf06cba7fc82a6f2406e5
成凯 <hellochengkai@hotmail.com>
2020-01-16 19:18:49 +0800

    修改ifdev变量的写入

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ write_config(){
     write_file="$HOME/.$1rc"
     if [ -f "$write_file" ]; then
         write_line "#add by ck" $write_file
-        write_line "export ifdev=wlo1" $write_file
+        write_line "export ifdev=" $write_file
         write_line "export LinuxConfigBoxPath=$PWD" $write_file
         write_line 'source $LinuxConfigBoxPath/shell/'"$1.sh" $write_file
     fi
